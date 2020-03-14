445131436214a1fcb9643af0cf04ff7775c506c4
Jordan <48123323+JordanSOA@users.noreply.github.com>
2020-03-14 17:44:31 +0100

    [DEV] Baddest Commit ever done, (to be deleted,squashed)

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,9 @@
 var Town = /** @class */ (function () {
-    function Town(name, countryMap, coord, route) {
+    function Town(name, countryMap, coord) {
+        this.routes = [];
         this.name = name;
         this.countryMap = countryMap;
         this.coord = coord;
-        this.routes = route;
     }
     Town.prototype.draw = function (ctx) {
         ctx.fillRect(this.coord.x, this.coord.y, 10, 10);
@@ -39,6 +39,7 @@ var Coord = /** @class */ (function () {
 }());
 var CountryMap = /** @class */ (function () {
     function CountryMap(width, height, backImg, canvas, towns) {
+        this.name = "France";
         this.width = width;
         this.height = height;
         this.backImg = backImg;
@@ -48,28 +49,52 @@ var CountryMap = /** @class */ (function () {
     CountryMap.prototype.draw = function () {
         // var canvas = document.createElement("canvas");
         // var ctx: CanvasRenderingContext2D = canvas.getContext("2d");
+        // var urlSrc: CanvasImageSource = "C:/Users/Workspace/Desktop/CoursSimplon/TSProject/svg/Départements_de_France-simple.svg";
+        // ctx.drawImage(urlSrc,0,0);
         //var reader:FileReader = new FileReader();
     };
     return CountryMap;
 }());
+var mCoor = new Coord(12, 99);
+var map = new CountryMap(150);
+var marseille = new Town("Marseille", map, mCoor);
+var aix = new Town("Aix", map, mCoor);
+var betwenR = new Route(222, marseille, aix);
+marseille.routes.push(betwenR);
+aix.routes.push(betwenR);
+map.towns = [marseille, aix];
+console.log(map);
 window.onload = function () {
-    var franceMap = new CountryMap(100);
-    franceMap.draw();
-    var divTest = document.createElement("div");
-    var formHtml = "<div>\n                                <form id=\"myForm\">\n                                    <input type=\"string\" id=\"fromTown\" >\n                                    <input type=\"string\" id=\"toTown\" >\n                                    <input type=\"submit\" id=\"resInput\" onclick=\"(fromTown.value)\"value=\"submit\">\n                                </form>\n                                <div id=\"chosenCityies\">You chose </div>\n                            </div>";
-    divTest.innerHTML = formHtml;
-    window.document.body.appendChild(divTest);
+    // var mapImg: string = `<img id="map" width="max-content" height="max-content" src="https://upload.wikimedia.org/wikipedia/commons/b/b6/D%C3%A9partements_de_France-simple.svg">`;
+    // var divImgBal = this.document.createElement("div");
+    // divImgBal.innerHTML = mapImg;
+    // window.document.body.appendChild(divImgBal);
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+    var dImgBal = this.document.createElement("img");
+    dImgBal.src = "https://upload.wikimedia.org/wikipedia/commons/b/b6/D%C3%A9partements_de_France-simple.svg";
+    //dImgBal.setAttribute('style',"visibility : hidden;");
+    this.document.body.appendChild(dImgBal);
+    //ctx.drawImage(dImgBal, 0, 0);
+    ctx.fillRect(250, 150, 10, 10);
+    this.document.body.appendChild(canvas);
 };
-window.addEventListener("submit", function (e) {
-    var _a;
-    console.log((_a = this.document.querySelector("#fromTown")) === null || _a === void 0 ? void 0 : _a.innerHTML);
-    e.preventDefault();
-});
-// window.addEventListener("onloadend", function(){
-//     console.log("ok");
+// window.onload = function () {
 //     var franceMap: CountryMap = new CountryMap(100);
 //     franceMap.draw();
 //     var divTest: HTMLDivElement = document.createElement("div");
-//     divTest.innerHTML = `<input type="file" id="files" name="files[]" accept=".json" multiple />`
+//     var formHtml: string = `<div>
+//                                 <form id="myForm">
+//                                     <input type="string" id="fromTown" >
+//                                     <input type="string" id="toTown" >
+//                                     <input type="submit" id="resInput" onclick="(fromTown.value)"value="submit">
+//                                 </form>
+//                                 <div id="chosenCityies">You chose </div>
+//                             </div>`;
+//     divTest.innerHTML = formHtml;
 //     window.document.body.appendChild(divTest);
+// }
+// window.addEventListener("submit", function (e) {
+//     console.log(this.document.querySelector("#fromTown")?.nodeValue);
+//     e.preventDefault();
 // })
--- scripts/index.ts
+++ scripts/index.ts
@@ -3,13 +3,12 @@ class Town {
     name: string;
     countryMap: CountryMap;
     coord: Coord;
-    routes: Route[];
+    routes: Route[] = [];
 
-    constructor(name: string, countryMap: CountryMap, coord: Coord, route: Route[]) {
+    constructor(name: string, countryMap: CountryMap, coord: Coord) {
         this.name = name;
         this.countryMap = countryMap;
         this.coord = coord;
-        this.routes = route;
     }
 
     draw(ctx: CanvasRenderingContext2D) {
@@ -30,7 +29,7 @@ class Route {
     }
 
     findTown(a: any, b: any) {
-        var obj: any ={
+        var obj: any = {
             a: a,
             b: b
         }
@@ -55,6 +54,7 @@ class Coord {
 }
 
 class CountryMap {
+    name: string = "France";
     width: number;
     height?: number;
     backImg?: CanvasImageSource;
@@ -72,37 +72,57 @@ class CountryMap {
     draw() {
         // var canvas = document.createElement("canvas");
         // var ctx: CanvasRenderingContext2D = canvas.getContext("2d");
+        // var urlSrc: CanvasImageSource = "C:/Users/Workspace/Desktop/CoursSimplon/TSProject/svg/Départements_de_France-simple.svg";
+        // ctx.drawImage(urlSrc,0,0);
         //var reader:FileReader = new FileReader();
     }
 }
 
+const mCoor: Coord = new Coord(12, 99);
+const map: CountryMap = new CountryMap(150);
+const marseille: Town = new Town("Marseille", map, mCoor);
+const aix: Town = new Town("Aix", map, mCoor);
+const betwenR: Route = new Route(222, marseille, aix);
+
+
+marseille.routes.push(betwenR);
+aix.routes.push(betwenR);
+map.towns = [marseille, aix];
+console.log(map);
 window.onload = function () {
-    var franceMap: CountryMap = new CountryMap(100);
-    franceMap.draw();
-    var divTest: HTMLDivElement = document.createElement("div");
-    var formHtml: string = `<div>
-                                <form id="myForm">
-                                    <input type="string" id="fromTown" >
-                                    <input type="string" id="toTown" >
-                                    <input type="submit" id="resInput" onclick="(fromTown.value)"value="submit">
-                                </form>
-                                <div id="chosenCityies">You chose </div>
-                            </div>`;
-    divTest.innerHTML = formHtml;
-    window.document.body.appendChild(divTest);
+    // var mapImg: string = `<img id="map" width="max-content" height="max-content" src="https://upload.wikimedia.org/wikipedia/commons/b/b6/D%C3%A9partements_de_France-simple.svg">`;
+    // var divImgBal = this.document.createElement("div");
+    // divImgBal.innerHTML = mapImg;
+    // window.document.body.appendChild(divImgBal);
+
+
+    var canvas = <HTMLCanvasElement> document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+    var dImgBal = this.document.createElement("img");
+    dImgBal.src = `https://upload.wikimedia.org/wikipedia/commons/b/b6/D%C3%A9partements_de_France-simple.svg`;
+    //dImgBal.setAttribute('style',"visibility : hidden;");
+    this.document.body.appendChild(dImgBal);
+    //ctx.drawImage(dImgBal, 0, 0);
+    ctx.fillRect(250, 150, 10, 10);
+    this.document.body.appendChild(canvas);
+
 }
-window.addEventListener("submit", function (e) {
-    console.log(this.document.querySelector("#fromTown")?.nodeValue);
-    e.preventDefault();
-})
-
-
-
-// window.addEventListener("onloadend", function(){
-//     console.log("ok");
+// window.onload = function () {
 //     var franceMap: CountryMap = new CountryMap(100);
 //     franceMap.draw();
 //     var divTest: HTMLDivElement = document.createElement("div");
-//     divTest.innerHTML = `<input type="file" id="files" name="files[]" accept=".json" multiple />`
+//     var formHtml: string = `<div>
+//                                 <form id="myForm">
+//                                     <input type="string" id="fromTown" >
+//                                     <input type="string" id="toTown" >
+//                                     <input type="submit" id="resInput" onclick="(fromTown.value)"value="submit">
+//                                 </form>
+//                                 <div id="chosenCityies">You chose </div>
+//                             </div>`;
+//     divTest.innerHTML = formHtml;
 //     window.document.body.appendChild(divTest);
+// }
+// window.addEventListener("submit", function (e) {
+//     console.log(this.document.querySelector("#fromTown")?.nodeValue);
+//     e.preventDefault();
 // })
